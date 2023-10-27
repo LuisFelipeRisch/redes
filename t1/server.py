bd101cebb5309b1cae6a0c7650f0714fbd752560
Luis Felipe Risch <luis.risch@gmail.com>
2023-10-26 21:26:45 -0300

    wip: improving client-serving logic

--- t1/server.py
+++ t1/server.py
@@ -1,42 +1,56 @@
 import socket
 import threading
-import json
 
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDRESS = (SERVER, PORT)
-DISCONNECT_MESSAGE = '!DISCONNECT'
+DISCONNECT_IDENTIFIER = 0
+BUFFER_SIZE = 1024
+MEDIA_PATH = 'moves/move1.mp4'
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 server.bind(ADDRESS)
 
-def handle_client(message, address):
+def handle_client(encoded_data, address):
   print(f"[NEW CONNECTION] {address} wants to receive the full video")
 
-  message = message.decode()
+  data = encoded_data.decode()
 
-  print(f"[{address}] {message}")
+  print(f"[{address}] {data}")
 
-  data = {"key1": "value1", "key2": "value2", "key3": 42}
-  json_data = json.dumps(data)
+  with open(MEDIA_PATH, 'rb') as media:
+    package_count = 1
 
-  server.sendto(json_data.encode(), address)
+    while True:
+      media_data = media.read(BUFFER_SIZE - 4)
 
-  server.sendto(DISCONNECT_MESSAGE.encode(), address)
+      if media_data:
+        data = package_count.to_bytes(4, 'big')
+        data += media_data
+
+        server.sendto(data, address)
+
+        package_count += 1
+      else:
+        break
+
+  print(f"[SERVER] Finished to send Media to {address}")
+
+  data = DISCONNECT_IDENTIFIER.to_bytes(4, 'big')
+
+  server.sendto(data, address)
 
 def start_server():
   print(f"[SERVER] server is running in address: {SERVER} on port: {PORT}")
 
   while True:
-    message, address = server.recvfrom(1024)
+    encoded_data, address = server.recvfrom(BUFFER_SIZE)
 
-    thread = threading.Thread(target=handle_client, args=(message, address))
+    thread = threading.Thread(target=handle_client, args=(encoded_data, address))
     thread.start()
 
     print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
-
-  pass
 
 print("[SERVER] is starting...")
 
